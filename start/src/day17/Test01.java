06c437b0cac1f4cafb2b15a306a5d313c7d2a0f9
kingnutbread <dohan2u@gmail.com>
2022-03-22 10:54:05 +0900

    ㅇㄹㅇ

---
+++ start/src/day17/Test01.java
@@ -0,0 +1,15 @@
+package day17;
+/*
+    파일에 내용을 저장하는 프로그램을 만들어보자.
+ */
+import java.io.*;
+public class Test01 {
+
+	public Test01() {
+	}
+
+	public static void main(String[] args) {
+		new Test01();
+	}
+
+}
